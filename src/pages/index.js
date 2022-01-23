acedead4b30161369b52337ade7f7e85051792ff
Sarah Rainsberger <sarah@rainsberger.ca>
2022-01-23 15:44:47 +0000

    updated message

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <Seo title="Home" />
     <h1>Hi TheJam.dev!!!</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Now go build something great... right in your browser!</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
